378c82eff6bfe8114fb213ccec98b8cb784623fa
wangggong <793160615@qq.com>
2020-10-13 18:24:43 +0800

    first ci
    In the beginning there was darkness

---
+++ config.el
@@ -0,0 +1,142 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "wangruichao"
+      user-mail-address "wangruichao2014@xiaochuankeji.cn")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "monospace" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'wheatgrass) ;; Green!
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/.doom.d")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+;; Least keymaps.
+(map! :nv "C-h" 'evil-window-left
+      :nv "C-j" 'evil-window-down
+      :nv "C-k" 'evil-window-up
+      :nv "C-l" 'evil-window-right
+      :n  "C-i" 'better-jumper-jump-forward
+      :n  "ZQ"  '+workspace/close-window-or-workspace
+
+      :leader
+      :n "w/"    'evil-window-vsplit
+      :n "w-"    'evil-window-split
+      :n "wT"    '+workspace/new
+      :n "<SPC>" 'counsel-M-x
+      :n "'"     '+vterm/toggle
+      :n "/"     '+default/search-project-for-symbol-at-point
+      :n "="     '+treemacs/toggle
+
+      :n "0" '+workspace/switch-to-final
+      :n "1" '+workspace/switch-to-0
+      :n "2" '+workspace/switch-to-1
+      :n "3" '+workspace/switch-to-2
+      :n "4" '+workspace/switch-to-3
+      :n "5" '+workspace/switch-to-4
+      :n "6" '+workspace/switch-to-5
+      :n "7" '+workspace/switch-to-6
+      :n "8" '+workspace/switch-to-7
+      :n "9" '+workspace/switch-to-8
+      )
+
+;; My rulers!
+(setq fci-rule-width 6)
+(setq fci-limit 100) ;; It's history.
+;; (setq fci-rule-color "blue")
+(add-hook! 'prog-mode-hook 'turn-on-fci-mode)
+
+;; VTerm
+(after! vterm (map! :map vterm-mode-map "C-c C-v" 'vterm-yank))
+
+(defun doom-dashboard-widget-banner ()
+  ;; Replace it to get my own dashboard!
+  (let ((point (point)))
+    (mapc (lambda (line)
+            (insert (propertize (+doom-dashboard--center +doom-dashboard--width line)
+                                'face 'doom-dashboard-banner) " ")
+            (insert "\n"))
+          '("     #######    ##  ##     ##                   ##        #######                 "
+            "     #######    ##  ##     ##                   ##        #######                 "
+            "    ##    ##       ##   # ##   #  #####       ## #       ##      ## ## ##   ##### "
+            "   ##    ##   ##  ##  #  ##  #  ##   ##       ##        ####    ### ##  # ##   ## "
+            "  #######    ##  ####   ####   ##   ##     ## ##       ##      ##  ##  # ##   ##  "
+            "  #######    ##  ####   ####   ##   ##     ## ##       ##      ##  ##  # ##   ##  "
+            " ##    ##   ##  ##  #  ##  #  ##   ##    ##   ## #    ##      ##  ##  # ##   ##   "
+            "##     ## #### ##   # ##   #  #### #    ##   ###     ####### ##      #  #### #    "
+            "                                        ####  ##                                  "
+            "                                        ####  ##                                  "))
+    (when (and (display-graphic-p)
+               (stringp fancy-splash-image)
+               (file-readable-p fancy-splash-image))
+      (let ((image (create-image (fancy-splash-image-file))))
+        (add-text-properties
+         point (point) `(display ,image rear-nonsticky (display)))
+        (save-excursion
+          (goto-char point)
+          (insert (make-string
+                   (truncate
+                    (max 0 (+ 1 (/ (- +doom-dashboard--width
+                                      (car (image-size image nil)))
+                                   2))))
+                   ? ))))
+      (insert (make-string (or (cdr +doom-dashboard-banner-padding) 0)
+                           ?\n)))))
+
+;; A transparent Ema!
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(set-frame-parameter (selected-frame) 'alpha '(75 50))
+
+;; Org mode:
+(after! org
+  (setq org-todo-keywords '((sequence "TODO(t)" "PLANING(p)" "INPROGRESS(i)" "BLOCK(b)" "|" "DONE(d)" "CANCELLED(c)"))
+        org-todo-keyword-faces '(("TODO" :foreground "#7c7c75" :weight normal: underline t)
+                                 ("PLANING" :foreground "#b0c4de" :weight normal: underline t)
+                                 ("INPROGRESS" :foreground "#0098dd" :weight normal: underline t)
+                                 ("BLOCK" :foreground "#9f7efe" :weight normal: underline t)
+                                 ("DONE" :foreground "#50a14f" :weight normal: underline t)
+                                 ("CANCELLED" :foreground "#ff6480" :weight normal: underline t)
+                                 )
+        org-log-done 'time)
+  )
---
+++ packages.el
@@ -0,0 +1,66 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+;; To install a package with Doom you must declare them here, run 'doom sync' on
+;; the command line, then restart Emacs for the changes to take effect.
+;; Alternatively, use M-x doom/reload.
+
+(package! fill-column-indicator)
+(package! php-cs-fixer)
+
+;; Doom's packages are pinned to a specific commit and updated from release to
+;; release. The `unpin!' macro allows you to unpin single packages...
+;(unpin! pinned-package)
+;; ...or multiple packages
+;(unpin! pinned-package another-pinned-package)
+;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
+;(unpin! t)
+
+
+;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
+;(package! some-package)
+
+;; To install a package directly from a particular repo, you'll need to specify
+;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
+;; https://github.com/raxod502/straight.el#the-recipe-format
+;(package! another-package
+;  :recipe (:host github :repo "username/repo"))
+(package! phpactor
+  :recipe (:host github :repo "emacs-php/phpactor.el"
+           :files ("*.el" "composer.json" "composer.lock" (:exclude "*test.el"))))
+
+;; If the package you are trying to install does not contain a PACKAGENAME.el
+;; file, or is located in a subdirectory of the repo, you'll need to specify
+;; `:files' in the `:recipe':
+;(package! this-package
+;  :recipe (:host github :repo "username/repo"
+;           :files ("some-file.el" "src/lisp/*.el")))
+
+;; If you'd like to disable a package included with Doom, for whatever reason,
+;; you can do so here with the `:disable' property:
+;; dotspacemacs-excluded-packages '(helm-c-yasnippet eshell-z eshell-prompt-extras
+;; esh-help evil-nerd-commenter evil-tutor restart-emacs open-junk-file password-generator
+;; highlight-numbers lorem-ipsum google-translate highlight-indentation golden-ratio
+;; fancy-battery evil-cleverparens doom-modeline all-the-icons org-bullets multi-term
+;; magit-svn gnuplot winum uuidgen ws-butler writeroom-mode memoize visual-fill-column)
+;;
+;; TODO: fill the list!
+(package! evil-nerd-commenter :disable t)
+;; (package! restart-emacs :disable t) ;; core
+;; (package! highlight-numbers :disable t)
+(package! highlight-quoted :disable t)
+(package! doom-modeline :disable t) ;; seems not useful
+(package! multiple-cursors :disable t)
+(package! emacs-memoize :disable t)
+
+
+;; You can override the recipe of a built in package without having to specify
+;; all the properties for `:recipe'. These will inherit the rest of its recipe
+;; from Doom or MELPA/ELPA/Emacsmirror:
+;(package! builtin-package :recipe (:nonrecursive t))
+;(package! builtin-package-2 :recipe (:repo "myfork/package"))
+
+;; Specify a `:branch' to install a package from a particular branch or tag.
+;; This is required for some packages whose default branch isn't 'master' (which
+;; our package manager can't deal with; see raxod502/straight.el#279)
+;(package! builtin-package :recipe (:branch "develop"))
